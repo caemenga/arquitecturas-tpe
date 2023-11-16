d2ed4903ec8e15e0aae410b56693f8c24e7efc76
Caetano <gmengarelliyespersen@alumnos.exa.unicen.edu.ar>
2023-11-16 14:07:20 -0300

    config doc y router

---
+++ microservicioSeguridad/src/main/java/com/example/entity/Authority.java
@@ -0,0 +1,28 @@
+package com.example.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Authority implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @NotNull
+    @Size(max = 50)
+    @Id
+    @Column(length = 50)
+    private String name;
+}
