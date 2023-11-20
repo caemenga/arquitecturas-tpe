bf53d417abddf4cfcd1cc990ec663fd82084e804
Caetano <gmengarelliyespersen@alumnos.exa.unicen.edu.ar>
2023-11-20 15:37:53 -0300

    merge ramas

---
+++ microservicioAdministrador/src/main/java/org/app/administrador/swagger/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package org.app.administrador.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@OpenAPIDefinition
+@Configuration
+public class SwaggerConfig {
+
+
+    @Bean
+    public OpenAPI api(){
+        return new OpenAPI().info(new Info().title("MONOPATINES")
+                .version("1.0-SNAPSHOT").description("APP DE MONOPATINES"));
+
+    }
+
+}
+
+//import org.springframework.context.annotation.Bean;
+//        import org.springframework.context.annotation.Configuration;
+//        import springfox.documentation.builders.PathSelectors;
+//        import springfox.documentation.builders.RequestHandlerSelectors;
+//        import springfox.documentation.spi.DocumentationType;
+//        import springfox.documentation.spring.web.plugins.Docket;
+//
+//@Configuration
+//public class SwaggerConfig {
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.OAS_30)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("org.app.usuarios.swagger"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//}
